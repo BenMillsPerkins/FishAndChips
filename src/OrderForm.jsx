c2ddb76e869d83ca1e9cdff9fc0711889db9ecc3
BenMillsPerkins <bengmp@gmail.com>
2018-11-29 15:59:13 +0000

    Updated input to not be an input field

--- src/OrderForm.jsx
+++ src/OrderForm.jsx
@@ -213,7 +213,7 @@ function MenuItem(props) {
             <div className="order-buttons">
                 <div className="input-buttons">
                     <button className="increment-order-button take-from-order" type="button" name={props.input} onClick = {() => props.falseMinusFromOrder(props.input)}><i className="fa fa-minus"></i></button>
-                    <input className="font16 order-input-field" id="NSinput" type="number" min="0" name={props.input} onChange={props.updateFalseOrder} value={props.falseOrder[props.input]}/>
+                    <label className="font16 input-field" id="NSinput" name={props.input} onChange={props.updateFalseOrder} value={props.falseOrder[props.input]}> {props.falseOrder[props.input]} </label>
                     <button className="increment-order-button add-to-order" type="button" name={props.input} onClick = {() => props.falseAddToOrder(props.input)}><i className="fa fa-plus"></i></button>
                 </div>
                 <div className="order-update-buttons">
