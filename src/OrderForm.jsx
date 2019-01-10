fecfe5a5446a8f929c72f68aa882bc2fcafbbfe6
Nathan Jonah <nathan.jonah@agepartnership.com>
2019-01-10 23:30:45 +0000

    Add more orders, remove existing classes and add bulma modifiers

--- src/OrderForm.jsx
+++ src/OrderForm.jsx
@@ -74,7 +74,7 @@ class OrderForm extends Component {
     }
 
     renderStage1() {
-        const menuItems = ['Team Chips', 'Fish', 'Fish Butty', 'Battered Sausage', 'Plain Sausage', 'Battered Sausage Butty', 'Cheesy Chips', 'Chip Butty', 'Battered Sausage Butty', 'Curry Sauce', 'Mushy Peas', 'Gravy', 'Fish and Chips', 'Small Chips', 'Scampi', 'Pie: Steak n Kidney (Chip Shop)', 'Pie: Meat (Chip Shop)', 'Pie: Beef and Onion (Chip Shop)', 'Pie: Chicken and Mushroom (Chip Shop)', 'Pie: Steak n Kidney (Pie Shop)', 'Pie: Meat (Pie Shop)', 'Pie: Beef and Onion (Pie Shop)', 'Pie: Chicken and Mushroom (Pie Shop)'];
+        const menuItems = ['Team Chips', 'Fish', 'Fish Butty', 'Battered Sausage', 'Plain Sausage', 'Battered Sausage Butty', 'Cheesy Chips', 'Chip Butty', 'Battered Sausage Butty', 'Curry Sauce', 'Mushy Peas', 'Gravy', 'Fish and Chips', 'Small Chips', 'Scampi', 'Steak n Kidney Pie (Chip Shop)', 'Meat Pie (Chip Shop)', 'Beef and Onion Pie (Chip Shop)', 'Chicken and Mushroom Pie (Chip Shop)', 'Steak n Kidney Pie (Pastry Shop)', 'Meat Pie (Pastry Shop)', 'Beef and Onion Pie (Pastry Shop)', 'Chicken and Mushroom Pie (Pastry Shop)'];
         return Object.entries(menuItems).map(([key, value]) => {
             return (
                 this.state.order[value] ? ''
@@ -121,9 +121,9 @@ class OrderForm extends Component {
         const order = this.state.order;
         return Object.entries(order).map(([key, value]) => {
 			return (
-				<div key={key}>
-					Item: {key}: {value}
-				</div>
+                <div key={key}>
+                    Item: {key}: {value}
+                </div>
 			)
 		})
     }
@@ -139,7 +139,7 @@ class OrderForm extends Component {
         submitDisabled = initials ? false : true;
 
         return (
-            <div>
+            <div className="container main-content">
                 { this.state.stage === 0
                     ? <Landing
                     initials = {this.state.initials}
@@ -149,44 +149,55 @@ class OrderForm extends Component {
                 }
                 { this.state.stage === 1
                     ?
-                    <div>
-                        <div className="inactive-order"> {this.renderStage1()} </div>
-                        <div className="basket"> {this.renderBasket()} </div>
+                    <div className="columns">
+                        <div className="column is-mobile inactive-order"> {this.renderStage1()} </div>
+                        <div className="column is-mobile basket"> {this.renderBasket()} </div>
                     </div>
                     : null
                 }
                 { this.state.stage === 2
-                    ? this.renderOrder()
+                    ? <article className="message is-warning">
+                        <div className="message-header">
+                            <p>Is this your order?</p>
+                        </div>
+                        <div className="message-body">
+                            {this.renderOrder()}
+                        </div>
+                    </article>
                     : null
                 }
                 { this.state.stage === 3
                     ? <div>
-                        <div className='thanks'>
-                            Thanks, your order has been placed!
-                        </div>
+                        <article className="message is-success">
+                            <div className="message-header">
+                                <p>Confirmed!</p>
+                            </div>
+                            <div className="message-body">
+                                Thanks, your order has been placed!
+                            </div>
+                        </article>
                         <div>
-                        <button className="button" type="button" onClick = {() => this.refreshPage()}> See the full order</button>
+                            <button className="button is-info full-order" type="button" onClick = {() => this.refreshPage()}><i className="fa fa-list"></i> See the full order</button>
                         </div>
                     </div>
                     : null
                 }
-                <br className="clear" />
-                <div className="button-row">
+                <div className="container main-content main-content--initials">
                     {this.state.stage !== 0 && this.state.stage !== 3
-                        ? <button className="back-button" type="button" onClick = {() => this.setState({stage: this.state.stage - 1})}><i className="fa fa-chevron-left"></i> Back</button>
+                        ? <button className="button is-info back-button" type="button" onClick = {() => this.setState({stage: this.state.stage - 1})}><i className="fa fa-chevron-left"></i> Back</button>
                         : null
                     }
                     { this.state.stage == 0
-                        ? <button className="button is-warning" type="button" disabled={submitDisabled} onClick = {() => this.setState({stage: this.state.stage + 1})}>On To Order <i className="fa fa-chevron-right"></i></button>
+                        ? <button className="button is-info to-order" type="button" disabled={submitDisabled} onClick = {() => this.setState({stage: this.state.stage + 1})}>On To Order <i className="fa fa-chevron-right"></i></button>
                         : null
                     }
                     { this.state.stage == 1
-                        ? <button className="button is-success" type="button" onClick = {() => this.setState({stage: this.state.stage + 1})}>Finish Order <i className="fa fa-chevron-right"></i></button>
+                        ? <button className="button is-success finish-order" type="button" onClick = {() => this.setState({stage: this.state.stage + 1})}>Finish Order <i className="fa fa-chevron-right"></i></button>
                         : null
                     }
                     {
                         this.state.stage == 2
-                        ? <button className="button" type="button" onClick = {() => this.sendOrder()}>Yes, this is my order <i className="fa fa-chevron-right"></i></button>
+                        ? <button className="button is-success confirm-order" type="button" onClick = {() => this.sendOrder()}>Yes, this is my order <i className="fa fa-chevron-right"></i></button>
                         : null
                     }
                </div>
@@ -197,11 +208,11 @@ class OrderForm extends Component {
 
 function Landing(props) {
     return (
-        <div>
-        <div className="gutter-bottom-10">
-            <label className="font16" htmlFor="initInput">Initials: </label>
-            <input className="font16" id="initInput" type="text" name="initials" onChange={props.update} value={props.initials}/>
-        </div>
+        <div className="container main-content">
+            <div className="gutter-bottom-10">
+                <label className="is-size-6 has-text-weight-bold" htmlFor="initInput">Initials: </label>
+                <input className="is-size-6" id="initInput" type="text" name="initials" onChange={props.update} value={props.initials}/>
+            </div>
         </div>
     )
 }
@@ -209,17 +220,36 @@ function Landing(props) {
 function MenuItem(props) {
     return (
         <div>
-            <label className="font16" htmlFor={props.input + "Input"}> {props.input} </label>
+            <p className="food-item-label" htmlFor={props.input + "Input"}>
+                <span className="has-text-weight-bold">{props.input}:</span> <label className="order-quantity" id="NSinput" name={props.input} onChange={props.updateFalseOrder} value={props.falseOrder[props.input]}>{props.falseOrder[props.input]}</label>
+            </p>
             <div className="order-buttons">
-                <div className="input-buttons">
-                    <button className="increment-order-button take-from-order" type="button" name={props.input} onClick = {() => props.falseMinusFromOrder(props.input)}><i className="fa fa-minus"></i></button>
-                    <label className="font16 input-field" id="NSinput" name={props.input} onChange={props.updateFalseOrder} value={props.falseOrder[props.input]}> {props.falseOrder[props.input]} </label>
-                    <button className="increment-order-button add-to-order" type="button" name={props.input} onClick = {() => props.falseAddToOrder(props.input)}><i className="fa fa-plus"></i></button>
-                </div>
-                <div className="order-update-buttons">
-                    <button className="add-to-order-button" type="button" name={props.input} onClick = {() => props.updateOrder(props.input)}> updateOrder </button>
+                <div className="input-buttons buttons has-addons">
+                    <button className="button is-info is-outlined increment-order-button take-from-order" type="button" name={props.input} onClick = {() => props.falseMinusFromOrder(props.input)}>
+                        <span className="icon is-small">
+                            <i className="fa fa-minus"></i>
+                        </span>
+                        <span>Less</span>
+                    </button>
+                    <button className="button is-info is-outlined increment-order-button add-to-order" type="button" name={props.input} onClick = {() => props.falseAddToOrder(props.input)}>
+                        <span className="icon is-small">
+                            <i className="fa fa-plus"></i>
+                        </span>
+                        <span>More</span>
+                    </button>
+                    <button className="button is-info is-outlined add-to-order-button" type="button" name={props.input} onClick = {() => props.updateOrder(props.input)}>
+                    <span class="icon is-small">
+                        <i class="fa fa-check"></i>
+                    </span>
+                    <span>Update Order</span>
+                    </button>
                     {!props.order[props.input] ? ''
-                        : <button className="remove-from-order-button" type="button" name={props.input} onClick = {() => props.removeFromOrder(props.input)}> Remove From Order </button>
+                        : <button className="button is-info is-outlined remove-from-order-button" type="button" name={props.input} onClick = {() => props.removeFromOrder(props.input)}>
+                            <span className="icon is-small">
+                                <i className="fa fa-trash"></i>
+                            </span>
+                            <span>Remove</span>
+                        </button>
                     }
                 </div>
             </div>
